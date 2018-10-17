d1de4ec7ab3f51cb72db399ce1cd60e4a154196c
K Lars Lohn <twobraids@gmail.com>
2018-10-17 14:42:46 -0700

    added SunTrigger with example

---
+++ demo/rule_system/evening_porchlight_rule.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import logging
+
+from datetime import (
+    datetime,
+    timedelta
+)
+
+from pywot.rules import (
+    Rule,
+    run_main,
+)
+from pywot.rule_triggers import (
+    SunTrigger
+)
+
+
+class EveningPorchLightRule(Rule):
+
+    def register_triggers(self):
+        sun_trigger = SunTrigger(
+            "sun_trigger",
+            "sunset",
+            (44.562951, -123.3535762),
+            "US/Pacific",
+            70.0,
+            600  # ten minutes
+        )
+        return (sun_trigger,)
+
+    def action(self, *args):
+        self.FrontPorchLight.on = True
+
+def main(config, rule_system):
+    my_rule = EveningPorchLightRule(
+        rule_system,
+        'turn on front porch light ten minutes after sunset'
+    )
+    rule_system.add_rule(my_rule)
+
+
+if __name__ == '__main__':
+    run_main(main)
+    logging.debug('done.')
--- pywot/rule_triggers.py
+++ pywot/rule_triggers.py
@@ -1,6 +1,6 @@
 import logging
 import asyncio
-
+import astral
 
 from datetime import (
     timedelta,
@@ -158,6 +158,57 @@ class AbsoluteTimeTrigger(TimeBasedTrigger):
             time_until_trigger_in_seconds = self.time_difference_in_seconds(
                 self.trigger_time,
                 datetime.now().time()
+            )
+            logging.debug('timer triggers in %sS', time_until_trigger_in_seconds)
+            await asyncio.sleep(time_until_trigger_in_seconds)
+            self._apply_rules('activated', True)
+            await asyncio.sleep(1)
+
+
+class SunTrigger(TimeBasedTrigger):
+    def __init__(
+        self,
+        name,
+        sun_event_name, # dawn, sunrise, noon, sunset, dusk
+        lat_long_tuple,
+        timezone_name,  # like "US/Pacific" or "GMT"
+        elevation_in_meters,
+        time_offset_in_seconds=0
+    ):
+        super(SunTrigger, self).__init__(name)
+        self.sun_event_name = sun_event_name
+        self.location = astral.Location((
+            "location_name",
+            "location_region",
+            lat_long_tuple[0],
+            lat_long_tuple[1],
+            timezone_name,
+            elevation_in_meters
+        ))
+        self.time_offset = timedelta(0, time_offset_in_seconds)
+        self.one_day = timedelta(1)
+
+    def get_sun_schedule(self, now):
+        try:
+            return self.location.sun(now)
+        except Exception:
+            # astral will raise exceptions if any of the sun events
+            # cannot be calculated - for example there is no sunrise
+            # in midwinter or sunset in mid summer in some locations.
+            # Guess by returning now + one day
+            return {self.sun_event_name: now + self.one_day}
+
+    async def trigger_detection_loop(self):
+        while True:
+            now = self.location.tz.localize(datetime.now())
+            sun_schedule = self.get_sun_schedule(now)
+            if sun_schedule[self.sun_event_name] + self.time_offset < now:
+                # this event is in the past, get tomorrow's event instead
+                sun_schedule = self.get_sun_schedule(now + self.one_day)
+
+            time_until_trigger_in_seconds = self.time_difference_in_seconds(
+                sun_schedule[self.sun_event_name] + self.time_offset,
+                now
             )
             logging.debug('timer triggers in %sS', time_until_trigger_in_seconds)
             await asyncio.sleep(time_until_trigger_in_seconds)
